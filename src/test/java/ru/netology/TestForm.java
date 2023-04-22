b4c24bc50e9a2e602a8489ddebf2a66f85aa012c
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-04-22 23:31:50 +0300

    add test+correct build.gradle

---
+++ src/test/java/ru/netology/TestForm.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TestForm {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void PositiveTestForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Петров Алеша");
+        $("[data-test-id='phone'] input").setValue("+79160567890");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+    }
+}
